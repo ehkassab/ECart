321d3baecc48e46edf13cbc533dcea3be12db3a6
MOCCAE\ehkassab <ehkassab@moccae.gov.ae>
2025-05-11 16:51:12 +0400

    Add Docker Compose support to ECart solution
    - Introduced a new Docker Compose project in `ECart.sln` for building and running the application in a Docker environment.
    - Updated solution configuration to integrate the Docker Compose project.
    - Modified `Catalog.API.csproj` to reference the Docker Compose project.
    - Enhanced `Program.cs` to add Marten service with PostgreSQL connection.
    - Updated `appsettings.json` with PostgreSQL connection string.
    - Created `docker-compose.dcproj` for Docker project settings.
    - Added `docker-compose.override.yml` for PostgreSQL service configuration.
    - Created `docker-compose.yml` to define services and volumes for Docker.
    - Updated `launchSettings.json` to include Docker Compose profile.
    
    Branch: feature/docker-compose

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -6,6 +6,12 @@ builder.Services.AddMediatR(config =>
         { 
             config.RegisterServicesFromAssembly(typeof(Program).Assembly);  
         });
+
+builder.Services.AddMarten(config =>
+{
+    config.Connection(builder.Configuration.GetConnectionString("CatalogDb")!);
+}).UseLightweightSessions();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

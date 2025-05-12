159e44027a90ef044f593a83712a0c08c7a9dd6a
MOCCAE\ehkassab <ehkassab@moccae.gov.ae>
2025-05-12 06:39:59 +0400

    Add product retrieval endpoints and exception handling
    - Introduced `ProductNotFoundException` for handling missing products.
    - Added `GetProductByIdEndpoint` to retrieve products by ID.
    - Implemented `GetProductByIdQuery` and handler for querying products.
    - Created `GetProductsEndPoint` to fetch all products.
    - Defined `GetProductsQuery` and handler for retrieving all products.
    - Updated `GlobalUsing.cs` to include new namespaces.
    
    Branch: feature/product-retrieval

--- src/Services/Catalog/Catalog.API/GlobalUsing.cs
+++ src/Services/Catalog/Catalog.API/GlobalUsing.cs
@@ -2,5 +2,8 @@
 global using Mapster;
 global using MediatR;
 global using BuildingBlocks.CQRS;
-global using Catalog.API.Models;
 global using Marten;
+
+
+global using Catalog.API.Models;
+global using Catalog.API.Exceptions;
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdHandler.cs
@@ -0,0 +1,28 @@
+﻿using Catalog.API.Products.GetProducts;
+
+namespace Catalog.API.Products.GetProductById
+{
+
+    public record GetProductByIdQuery(Guid Id) : IQuery<GetProductsByIdResult>;
+
+    public record GetProductsByIdResult(Product Product);
+
+    public class GetProductByIdQueryHandler (IDocumentSession session, ILogger<GetProductByIdQueryHandler> logger)
+        : IQueryHandler<GetProductByIdQuery, GetProductsByIdResult>
+    {
+        public async Task<GetProductsByIdResult> Handle(GetProductByIdQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByIdQuery handler called with {@query}", query);
+
+
+            var product = await session.LoadAsync<Product>(query.Id, cancellationToken);
+
+            if(product is null)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductsByIdResult(product);
+        }
+    }
+}
